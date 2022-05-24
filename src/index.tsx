77dba5faaf6709cfb4b36503d5e6cef0c787a6bc
Tom Grönwoldt <tom.groenwoldt@outlook.de>
2022-05-24 23:41:41 +0200

    Add mui and starting page

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const test = 6;
-
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
