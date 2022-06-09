f887f9bd53ee25bad8ee5df8921035bd8fd7c9d7
Tom Grönwoldt <tom.groenwoldt@outlook.de>
2022-06-10 00:19:49 +0200

    Add type restrictions

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,72 @@
-import React from "react";
+import { useState, useEffect } from 'react';
 import { TextField } from "@mui/material";
 import "./App.css";
 
-let sampleText =
-  "You clearly don't know who you're talking to, so let me clue you in."; // I am not in danger, Skyler. I am the danger. A guy opens his door and gets shot and you think that of me? No. I am the one who knocks!";
+type Message = {
+  room: string,
+  username: string,
+  position: number,
+};
 
-// initialization phase
-let currentWord = sampleText.split(" ")[0];
-let spaceAsNextWord = true;
-sampleText = sampleText.split(" ").slice(1).join(" ");
-
-const message = {
-  lobby: "lobby",
+const message: Message = {
+  room: "lobby",
   username: "guest",
   position: 0,
 };
 
-// TODO: add case for end of text
-function setCurrectWord() {
-  if (spaceAsNextWord) {
-    currentWord = " ";
-    spaceAsNextWord = false;
-    if (sampleText.length === 0) {
-      currentWord = "THE_END";
-    }
-  } else {
-    currentWord = sampleText.split(" ")[0];
-    sampleText = sampleText.split(" ").slice(1).join(" ");
-    spaceAsNextWord = true;
-  }
-}
-
-function validate(e: { target: { value: string } }) {
-  if (currentWord !== "THE_END") {
-    validateWord(e);
-    console.log(currentWord);
-    send();
-  }
-}
-
-function validateWord(e: { target: { value: string } }) {
-  if (e.target.value === currentWord) {
-    message.position += currentWord.length;
-    setCurrectWord();
-    e.target.value = "";
-  }
-}
-
-function send() {
-  fetch("http://localhost:8000/message", {
+async function send() {
+  await fetch("http://localhost:8000/message", {
     method: "POST",
-    body: new URLSearchParams({
-      room: message.lobby,
-      username: message.username,
-      position: message.position.toString(),
-    }),
+    body: JSON.stringify(message),
   });
 }
 
 function App() {
+
+  const [sampleText, setSampleText] = useState(
+    "You clearly don't know who you're talking to, so let me clue you in."); // I am not in danger, Skyler. I am the danger. A guy opens his door and gets shot and you think that of me? No. I am the one who knocks!";
+  const [currentWord, setCurrentWord] = useState("");
+  const [spaceAsNextWord, setSpaceAsNextWord] = useState(true);
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  function init() {
+    setCurrentWord(sampleText.split(" ")[0]);
+    setSampleText(sampleText.split(" ").slice(1).join(" "));
+  }
+
+  function setWord() {
+    if (spaceAsNextWord) {
+      setCurrentWord(" ");
+      setSpaceAsNextWord(false);
+      if (sampleText.length === 0) {
+        setCurrentWord("THE_END");
+      }
+    } else {
+      setCurrentWord(sampleText.split(" ")[0]);
+      setSampleText(sampleText.split(" ").slice(1).join(" "));
+      setSpaceAsNextWord(true);
+    }
+  }
+
+  function validate(e: { target: { value: string } }) {
+    if (currentWord !== "THE_END") {
+      validateWord(e);
+    }
+  }
+
+  function validateWord(e: { target: { value: string } }) {
+    if (e.target.value === currentWord) {
+      message.position += currentWord.length;
+      send();
+      setWord();
+      e.target.value = "";
+    }
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
