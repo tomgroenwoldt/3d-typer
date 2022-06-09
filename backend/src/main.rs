9abe2c86b21337dda6f46aec0d2cf66866d7d16b
Tom Grönwoldt <tom.groenwoldt@outlook.de>
2022-06-10 00:21:28 +0200

    Add CORS headers

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,6 @@
 #[macro_use]
 extern crate rocket;
-use rocket::tokio::sync::broadcast::channel;
+use rocket::{tokio::sync::broadcast::channel};
 
 mod message;
 mod routes;
@@ -10,7 +10,34 @@ use routes::{events, post};
 
 #[launch]
 fn rocket() -> _ {
+    use rocket::fairing::{Fairing, Info, Kind};
+    use rocket::http::Header;
+    use rocket::{Request, Response};
+
+    pub struct CORS;
+
+    #[rocket::async_trait]
+    impl Fairing for CORS {
+        fn info(&self) -> Info {
+            Info {
+                name: "Add CORS headers to responses",
+                kind: Kind::Response,
+            }
+        }
+
+        async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+            response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+            response.set_header(Header::new(
+                "Access-Control-Allow-Methods",
+                "POST, GET, PATCH, OPTIONS",
+            ));
+            response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+            response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+        }
+    }
+
     rocket::build()
         .manage(channel::<Message>(1024).0)
         .mount("/", routes![post, events])
+        .attach(CORS)
 }
