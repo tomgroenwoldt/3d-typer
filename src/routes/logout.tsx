98245ba1f1b47bd3cb05042343168334dfc60570
Tom Grönwoldt <tom.groenwoldt@outlook.de>
2022-07-12 11:50:18 +0200

    Add logout button

---
+++ src/routes/logout.tsx
@@ -0,0 +1,39 @@
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { AuthenticationProps } from "../auth";
+
+const theme = createTheme();
+
+export default function Logout(props: AuthenticationProps): JSX.Element {
+	async function logout() {
+		await fetch("http://localhost:8000/logout", {
+			method: "GET",
+			mode: "cors",
+			credentials: "include",
+			headers: {
+				Accept: "application/json",
+			},
+		}).catch(err => {
+			console.log("Error logging out: " + err);
+		});
+		if (props.setAuthentication !== undefined) {
+			props.setAuthentication(false);
+		}
+	}
+	return (
+		<ThemeProvider theme={theme}>
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<Button
+					fullWidth
+					variant="contained"
+					sx={{ mt: 3, mb: 2 }}
+					onClick={logout}>
+					Logout
+				</Button>
+			</Container>
+		</ThemeProvider>
+	);
+}
