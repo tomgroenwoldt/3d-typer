d83b607c99361ad587fe8fd17c2c3aadac964dff
tomgroenwoldt <tomgroenwoldt@users.noreply.github.com>
2022-07-14 21:43:23 +0000

    Prettified Code!

--- src/auth.ts
+++ src/auth.ts
@@ -5,14 +5,17 @@ export interface AuthenticationProps {
 export async function authenticate(
 	setAuthentication: (data: boolean) => void
 ): Promise<void> {
-	await fetch("https://" + process.env.REACT_APP_HOST + ":8000/authenticate", {
-		method: "GET",
-		mode: "cors",
-		credentials: "include",
-		headers: {
-			Accept: "application/json",
-		},
-	})
+	await fetch(
+		"https://" + process.env.REACT_APP_HOST + ":8000/authenticate",
+		{
+			method: "GET",
+			mode: "cors",
+			credentials: "include",
+			headers: {
+				Accept: "application/json",
+			},
+		}
+	)
 		.then(res => res.json())
 		.then((data: boolean) => {
 			setAuthentication(data);
